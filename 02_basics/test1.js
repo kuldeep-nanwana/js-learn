9b5010d0584458da60deedf831d230dd4af6a0cd
kuldeep-nanwana <kuldeepn2710@gmail.com>
2025-06-03 11:25:21 +0530

    Added test1.js and removed unused devcontainer and dependabot files

---
+++ 02_basics/test1.js
@@ -0,0 +1,20 @@
+accountEmail="an243457@gmail.com"
+let account_Designation="CEO"
+const account_id=303030
+
+// print multiple variables in a single statement
+console.table([account_Designation,account_id,accountEmail])
+
+/*Multiline
+commenting in 
+javascript */
+
+// typeof is used to give the data type of variables 
+console.log(typeof(account_Designation));
+
+// character or value can be directly passed to print the type of 
+console.log(typeof "");
+
+//type of null data type returns object value
+console.log(typeof null);
+
