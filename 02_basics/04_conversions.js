8ef0eb0fcad2cf8ae56fe497ca4e0a5f085d8563
kuldeep-nanwana <kuldeepn2710@gmail.com>
2025-06-04 16:37:07 +0530

    Added a js file to study the conversion and comparison between datatypes in js

---
+++ 02_basics/04_conversions.js
@@ -0,0 +1,24 @@
+// checking the conversion and comparisons of data types
+// console.log("2">1);   //true
+// console.log(23 =="23");  //true
+// console.log(24 < "23");   //false
+
+
+// conversion check of null and undefined
+
+
+console.log(undefined);
+
+// === operator compares data type as well as value
+console.log("2"=== "2" );
+// console.log("23"=== 3);
+
+
+
+
+
+
+
+
+
+
